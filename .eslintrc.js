16d4984226eba4d0868c9cc34302e7b07812f732
Liu Bergária <liubergaria@hotmail.com>
2021-04-20 21:51:25 -0300

    Project base configured

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  globals: {
+    __DEV__: 'readonly',
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/require-default-props': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/no-unescaped-entities': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
